e83b6946145ecf4b58a7bdc8ec4dfab6ca9cebc9
Walid-YAICH <wyaich@outlook.fr>
2017-01-20 18:45:05 +0100

    stable

--- src/main/java/javaee/ejb/CompteBancaire.java
+++ src/main/java/javaee/ejb/CompteBancaire.java
@@ -1,25 +1,36 @@
 package javaee.ejb;
 
-import javax.ejb.Stateful;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Stateless;
 
-@Stateful
+@Singleton
 public class CompteBancaire implements CompteBancaireLocal, CompteBancaireRemote{
-
+	
 	private int solde = 0;
-
+	
+	private String lastBeanUser;
+	
+	@PostConstruct
+	void postconstruct(){
+		lastBeanUser = "nobody";		
+	}
+	
 	@Override
 	public String versement(String nomPrenom, int montant) {
-		System.out.println("Le solde de " +nomPrenom + " est : " + solde +  " et il/elle veut verser " + montant);
 		solde = solde + montant;
-		return "Vous etes" + nomPrenom + ", votre nouveau Solde est : " + solde;
+		System.out.println("Le solde de " +nomPrenom + " est : " + solde +  " et il/elle veut verser " + montant);
+		String response = "Vous etes "+ nomPrenom +", Vous avez utilisé le bean de " + lastBeanUser + ", votre nouveau Solde est : " + solde;
+		lastBeanUser = nomPrenom;
+		return response;
 	}
 
 	@Override
 	public String retrait(String nomPrenom, int montant) {
+		lastBeanUser = nomPrenom;
 		System.out.println("Le solde de " +nomPrenom + " est : " + solde +  " et il/elle veut retirer " + montant);
 		solde = solde - montant;
-		return "Vous etes" + nomPrenom + ", votre nouveau Solde est : " + solde;
+		return "Vous etes "+ nomPrenom + ", Vous avez utilisé le bean de " + lastBeanUser + ", votre nouveau Solde est : " + solde;
 	}
-	
 
 }
